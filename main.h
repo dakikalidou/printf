128c6b071408623f1da59a9ff40fcf4aa7584166
Daki <kalidoudaki@gmail.com>
2022-04-19 23:49:40 +0000

    Features Files in Our Project

--- main.h
+++ main.h
@@ -1,17 +1,16 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef MAIN_H
+#define MAIN_H
 
-#include <stdlib>
-#include <stdarg>
+#include <stdlib.h>
+#include <stdarg.h>
 
 /**
- * struct flags - struct containing flag to "turn on"
+ * struct flags - struct containing flags to "turn on"
  * when a flag specifier is passed to _printf()
  * @plus: flag for the '+' character
- * @space: flag for the '' character
+ * @space: flag for the ' ' character
  * @hash: flag for the '#' character
  */
-
 typedef struct flags
 {
 	int plus;
@@ -25,54 +24,53 @@ typedef struct flags
  * @c: format specifier
  * @f: pointer to the correct printing function
  */
-
 typedef struct printHandler
 {
 	char c;
-	int (*f)(va_list ap. flag_t *f);
+	int (*f)(va_list ap, flags_t *f);
 } ph;
 
-/* printf_nums */
-int print_int(va_list 1, flags_t *f);
+/* print_nums */
+int print_int(va_list l, flags_t *f);
 void print_number(int n);
-int print_unsigned(va_list 1, flags_t *f);
-int count_digit (int i);
+int print_unsigned(va_list l, flags_t *f);
+int count_digit(int i);
 
-/* printf_bases */
-int print_hex(va_list 1, flags_t *f);
-int print_hex_big(va_list 1, flags_t *f);
-int print_binary(va_list 1, flags_t *f);
-int print_octal(va_list 1, flags_t *f);
+/* print_bases */
+int print_hex(va_list l, flags_t *f);
+int print_hex_big(va_list l, flags_t *f);
+int print_binary(va_list l, flags_t *f);
+int print_octal(va_list l, flags_t *f);
 
 /* converter */
-char *convert(unsigned long int num, int base, int base, int lowercase);
+char *convert(unsigned long int num, int base, int lowercase);
 
 /* _printf */
 int _printf(const char *format, ...);
 
-/* get_printf */
+/* get_print */
 int (*get_print(char s))(va_list, flags_t *);
 
 /* get_flag */
 int get_flag(char s, flags_t *f);
 
 /* print_alpha */
-int print_string(va_list 1, flags_t *f);
-int print_char(va_list 1, flags_t *f);
+int print_string(va_list l, flags_t *f);
+int print_char(va_list l, flags_t *f);
 
-/* write funcs */
+/* write_funcs */
 int _putchar(char c);
 int _puts(char *str);
 
-/* print_customs */
-int print_rot13(va_list 1, flags_t *f);
-int print_rev(va_list 1, flags_t *f);
-int print_bigS(va_list 1, flags_t *f);
+/* print_custom */
+int print_rot13(va_list l, flags_t *f);
+int print_rev(va_list l, flags_t *f);
+int print_bigS(va_list l, flags_t *f);
 
 /* print_address */
-int print_address(va_list 1, flags_t *f);
+int print_address(va_list l, flags_t *f);
 
 /* print_percent */
-int print_percent(va_list 1, flags_t *f);
+int print_percent(va_list l, flags_t *f);
 
 #endif
