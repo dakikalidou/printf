407df1fd398b454bc06ef33da01a360083a6a40f
Daki <kalidoudaki@gmail.com>
2022-04-20 15:10:04 +0000

    _printf.c

--- _printf.c
+++ _printf.c
@@ -3,15 +3,16 @@
 /**
  * _printf - produces output according to a format
  * @format: format string containing the characters and the specifiers
- * Description: this function will call the get_print() fuction that will
+ * Description: this function will call the get_print() function that will
+ * determine which printing function to call depending on the conversion
  * specifiers contained into fmt
- * Return: lenght of the formatted output string
- * */
+ * Return: length of the formatted output string
+ */
 int _printf(const char *format, ...)
 {
-	int (*pfunc)(va_list, flag_t *);
+	int (*pfunc)(va_list, flags_t *);
 	const char *p;
-	va_list argument;
+	va_list arguments;
 	flags_t flags = {0, 0, 0};
 
 	register int count = 0;
@@ -19,8 +20,8 @@ int _printf(const char *format, ...)
 	va_start(arguments, format);
 	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
-	if (format[0] == '%' && !format[1] == ' ' && !format[2])
-		return(-1)
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
 	for (p = format; *p; p++)
 	{
 		if (*p == '%')
@@ -37,7 +38,7 @@ int _printf(const char *format, ...)
 			count += (pfunc)
 				? pfunc(arguments, &flags)
 				: _printf("%%%c", *p);
-		} else 
+		} else
 			count += _putchar(*p);
 	}
 	_putchar(-1);
